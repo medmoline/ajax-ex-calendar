a44cb8145f98abd6269cd797f295389b591111d2
med_moline <antonio.medugno1990@gmail.com>
2019-05-24 17:51:10 +0200

    punto 3 e punto 4

--- ajax-ex-calendar/main.js
+++ ajax-ex-calendar/main.js
@@ -6,27 +6,33 @@
 $(document).ready(function() {
 //imposto un contatore mese che parte da 1
   var contatore_mese = 1;
-  mostra_mese();
+    mostra_mese();
+    chiamata_ajax();
 
     //quando clicco il bottone prev
-  $('.prev').click(function() {
-  //mese scende di un grado
-  //resetto l'html iniziale
-    $('.mese').html('')
-    contatore_mese--;
-    if(contatore_mese <= 12){
-      mostra_mese();
-    }
-  })
+    $(document).on('click', '.prev', function() {
+    //mese scende di un grado
+    //resetto l'html iniziale
+      $('.mese').html('')
+      contatore_mese--;
+      if(contatore_mese <= 12){
+        mostra_mese();
+        chiamata_ajax();
+      }
+    })
+
   //quando clicco il bottone next
-  $('.next').click(function() {
+  $(document).on('click', '.next', function() {
+    //resetto l'Html iniziale
     $('.mese').html('')
     //mese sale di un grado
     contatore_mese++;
-    if(contatore_mese >= 1){
-      mostra_mese();
-    }
-  })
+      if(contatore_mese >= 1){
+        mostra_mese();
+        chiamata_ajax();
+      }
+    })
+
 
   function mostra_mese(){
     //sostituisco il valore contatore nella data
@@ -39,6 +45,7 @@ $(document).ready(function() {
     //variabile per mese
     var mese_lettere = moment_date.format('MMMM');
     var mese_numero = moment_date.format('MM');
+    console.log(mese_numero);
     //variabile per anno
     var anno = moment_date.format('YYYY');
     //nel testo inizialmente vuoto inserisco il mese e l'anno
@@ -46,18 +53,45 @@ $(document).ready(function() {
 
     //ciclo da 1 fino alla lunghezza del mese
     for(var i = 1;i <= giorni; i++){
+      if( i < 10){
+        i = '0'+ i;
+      }
       var giorno = i + ' ' + mese_lettere;
       //giorno uguale al numero i che cicla di volta in volta e
       var source   = $("#entry-template").html();
       var template = Handlebars.compile(source);
       var context = {
         'title': giorno,
-        'body': anno + '-' + mese_numero +'-'+i
+        'body': anno + '-' + mese_numero +'-'+ i
       };
-      var html    = template(context);
+
+      var html = template(context);
+
       //appendo ogni volta nelle li
       $('.mese').append(html);
     }
 
   }
+
+function chiamata_ajax(){
+  var numero_mesi_anno = contatore_mese-1;
+  $.ajax({
+    'url': 'https:flynn.boolean.careers/exercises/api/holidays',
+    'data':{
+      'year': 2018,
+      'month': numero_mesi_anno
+    },
+    'success': function(data){
+      $('.mese li').each(function() {
+        console.log(data.response.length)
+        for(var i=0; i<data.response.length; i++){
+          if(data.response[i].date == $(this).data('li')){
+            $(this).append( '<p>'+ data.response[0].name +'</p>' ).addClass('red')
+          }
+        }
+      })
+    }
+  })
+}
+
 })
